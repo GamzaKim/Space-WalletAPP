1a10e658f3ea8d95b55abdb9537f7a984fb6d519
GamzaKim <lkcqwe@naver.com>
2023-07-03 17:11:35 +0900

    a

--- app/src/main/java/com/example/space_wallet/MainActivity.kt
+++ app/src/main/java/com/example/space_wallet/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.space_wallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val insertBtn = findViewById<Button>(R.id.insertBtn)
+        val searchBtn = findViewById<Button>(R.id.searchBtn)
+        val timeSearchBtn = findViewById<Button>(R.id.timeSearchBtn)
+
+
+        insertBtn.setOnClickListener {
+            val intent = Intent(this, InsertActivity::class.java)
+            startActivity(intent)
+        }
+
+        searchBtn.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        timeSearchBtn.setOnClickListener {
+            val intent = Intent(this, FindDateActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
+
 }
